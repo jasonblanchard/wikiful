fe3152d6a620077c690ec9dbac6ffb4433bb5f81
Jason Blanchard <jas.blanchard@gmail.com>
2014-04-19 10:33:07 -0400

    Added signup

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 feature 'Users' do
 
+  scenario 'As a curious author I want to be able to sign pu' do
+    visit new_user_path
+
+    fill_in 'user_email', :with => "test@example.com"
+    fill_in 'user_name', :with => "test"
+    fill_in 'user_password', :with => "testpass"
+    fill_in 'user_password_confirmation', :with => "testpass"
+    click_button "Create User"
+    
+    expect(page.body).to have_content "Thank you for signing up"
+  end
+
+  scenario 'As a curious author I want to see errors when the signup form fails' do
+    visit new_user_path
+
+    fill_in 'user_email', :with => "test@example.com"
+    fill_in 'user_name', :with => "test"
+    click_button "Create User"
+
+    expect(page.body).to have_content "Password can't be blank"
+  end
+
   scenario 'As an Author I want to be able to log in' do
     skip
   end
