d0bb40c9ffab70842bb6b77e4ce0ffe721b01165
Jason Blanchard <jas.blanchard@gmail.com>
2014-05-12 08:51:27 -0400

    Better welcome page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = User.articles.create(article_params)
+    @article = current_user.articles.create(article_params)
 
     if @article.save
       redirect_to @article
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @articles = Article.recent.limit(5)
   end
 end
